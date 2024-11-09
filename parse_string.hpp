e354ab96ca2af67c73037376a6b358ab61269566
LuwamG <30065081@cc.peralta.edu>
2024-11-09 00:00:44 -0800

    parse_string.hpp

--- parse_string.hpp
+++ parse_string.hpp
@@ -1,10 +1,11 @@
 #ifndef PARSE_STRING_HPP
 #define PARSE_STRING_HPP
 #include <iostream>
-
+#include <string>
 using namespace std;
 struct Movie {
 	string title;
 	string season_year;
 };
+Movie parse_string(const string& film);
 #endif
